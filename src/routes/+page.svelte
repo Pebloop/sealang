ce0891fdec64fae23fc3b9d3d4d8bd5b6a353adb
pebloop <pebloop@outlook.com>
2025-05-09 11:44:28 +0200

    Add main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,46 @@
+<div class="p-4">
+	<h2 class="text-3xl">Welcome to Sealang website !</h2>
+	<p>The place to learn how to write in sealang language.</p>
 
+	<br/>
+
+	<div>
+		<p class="text-2xl">What is Sealang?</p>
+		<p>Sealang is the language of the sea dragons of Edmayne. It is usually talked with the lights on their body,
+			which make it nearly impossible to talk for other species. However, it is entirely possible to write it down !<p/>
+	</div>
+
+	<hr class="my-4" />
+
+	<div class="flex flex-col w-full h-full items-center justify-center">
+		<div class="w-1/2 bg-gray-100 p-4 rounded-lg shadow-lg">
+			<a href="/grammar" class="text-blue-500 hover:text-blue-700">
+				<h2 class="text-2xl">Grammar</h2>
+				<p>Learn the grammar of Sealang.</p>
+			</a>
+		</div>
+
+		<div class="w-1/2 bg-gray-100 p-4 rounded-lg shadow-lg mt-4">
+			<a href="/dictionary" class="text-blue-500 hover:text-blue-700">
+				<h2 class="text-2xl">Dictionary</h2>
+				<p>Learn the vocabulary of Sealang.</p>
+			</a>
+		</div>
+
+		<div class="w-1/2 bg-gray-100 p-4 rounded-lg shadow-lg mt-4">
+			<a href="/history" class="text-blue-500 hover:text-blue-700">
+				<h2 class="text-2xl">History</h2>
+				<p>Learn about sealang history.</p>
+			</a>
+		</div>
+
+		<div class="w-1/2 bg-gray-100 p-4 rounded-lg shadow-lg mt-4">
+			<a href="/write" class="text-blue-500 hover:text-blue-700">
+				<h2 class="text-2xl">Write</h2>
+				<p>Write in Sealang.</p>
+			</a>
+		</div>
+
+	</div>
+
+</div>
